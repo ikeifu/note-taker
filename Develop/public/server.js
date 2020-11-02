294e2dcbc2ff2c82fd9106e6040f53166b3e7147
Keith Fung <fungsenkeith@gmail.com>
2020-11-01 21:47:14 -0500

    linked server with json

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -3,6 +3,10 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+const data = fs.readFile("db.json", "utf-8", (err, x) => {
+  if (err) throw err;
+  console.log(x);
+});
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -11,10 +15,6 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Star Wars Characters (DATA)
-// =============================================================
-var notes = [];
 
 // Routes
 // =============================================================
@@ -30,29 +30,14 @@ app.get("/notes", function (req, res) {
 
 // // Displays all characters
 app.get("/api/notes", function (req, res) {
-  return res.json(notes);
+  res.sendFile(path.join(__dirname, "db.json"));
 });
 
-// // Displays a single character, or returns false
-// app.get('/api/characters/:character', function (req, res) {
-//   var chosen = req.params.character;
-
-//   console.log(chosen);
-
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function (req, res) {
+// Create New Notes - takes in JSON input
+app.post("/api/notes", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newCharacter = req.body;
+  var newNotes = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
